d3c62468de357083d5230cb1cceb8fc74c7683d8
theog <theo.gaintza@etud.univ-pau.fr>
2025-02-06 14:47:58 +0100

    Commit initial

---
+++ RapidQuizerSpring/src/main/java/com/tgza/rapidquizerspring/RapidQuizerSpringApplication.java
@@ -0,0 +1,13 @@
+package com.tgza.rapidquizerspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RapidQuizerSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RapidQuizerSpringApplication.class, args);
+	}
+
+}
---
+++ RapidQuizerSpring/src/test/java/com/tgza/rapidquizerspring/RapidQuizerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tgza.rapidquizerspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RapidQuizerSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
