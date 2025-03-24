7a0c0fe81bf75b2522bdd12855dff99a60c7fb1e
TheTayti <taytisosuke@gmail.com>
2025-03-25 00:53:21 +0100

    Les inputs pour créer les réponses ne marchent pas

--- RapidQuizerAngular/src/app/app.routes.ts
+++ RapidQuizerAngular/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LatexToJsonComponent } from './components/latex-to-json/latex-to-json.component';
 import { QuestionListComponent } from './components/question-list/question-list.component';
+import { AddComponent } from './components/add/add.component';
 
 export const routes: Routes = [
   { path: 'latex-to-json', component: LatexToJsonComponent },
   { path: '', redirectTo: '/latex-to-json', pathMatch: 'full' },
-  { path: 'questions', component: QuestionListComponent }
+  { path: 'questions', component: QuestionListComponent },
+  { path: 'add', component: AddComponent }
 ];
