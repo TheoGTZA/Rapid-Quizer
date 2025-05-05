cc717bb5b58820cb0dd18873279786ba9abf021d
mathieu-gtz <mathieu.gaintza@gmail.com>
2025-05-05 21:44:46 +0200

    un utilisateur autre que user ou contributor ne peut plus acceder a la page ajout en rentrant l'url manuellement

--- RapidQuizerAngular/src/app/app-routing.module.ts
+++ RapidQuizerAngular/src/app/app-routing.module.ts
@@ -5,14 +5,15 @@ import {PanierComponent} from './components/panier/panier.component';
 import { LoginComponent } from './auth/components/login-component/login.component';
 import { RegisterComponent } from './auth/components/register/register.component';
 import { AuthGuard } from './auth/guards/auth/auth-guard.guard';
+import { AddComponent } from './components/add/add.component';
 
 const routes: Routes = [
   { path: '', component: LatexToJsonComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'panier', component: PanierComponent },
+  { path: 'add', component: AddComponent, canActivate: [AuthGuard] },  
   { path: '**', redirectTo: '' },
-  {path: 'add', component: LatexToJsonComponent, canActivate: [AuthGuard]}, 
 ];
 
 @NgModule({
--- RapidQuizerAngular/src/app/app.routes.ts
+++ RapidQuizerAngular/src/app/app.routes.ts
@@ -5,13 +5,15 @@ import { AddComponent } from './components/add/add.component';
 import {PanierComponent} from './components/panier/panier.component';
 import { RegisterComponent } from './auth/components/register/register.component';
 import { LoginComponent } from './auth/components/login-component/login.component';
+import { AuthGuard } from './auth/guards/auth/auth-guard.guard';
 
 export const routes: Routes = [
-  { path: 'latex-to-json', component: LatexToJsonComponent },
   { path: '', redirectTo: '/latex-to-json', pathMatch: 'full' },
+  { path: 'latex-to-json', component: LatexToJsonComponent },
   { path: 'questions', component: QuestionListComponent },
-  { path: 'add', component: AddComponent },
   { path: 'panier', component: PanierComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'add', component: AddComponent, canActivate: [AuthGuard] },
+  { path: '**', redirectTo: '/latex-to-json' } 
 ];
